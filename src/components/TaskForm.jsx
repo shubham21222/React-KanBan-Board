73c78d2413bc6983f1c9dbe23c35d2a0bb18cae2
shubham21222 <shubhamraikwar@hotmail.com>
2024-10-09 10:25:16 +0530

    fix input field issue

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,19 +1,23 @@
-// src/components/TaskForm.js
-
+import React from "react";
+import PropTypes from "prop-types";
 import Button from "./common/Button";
 import InputField from "./common/InputField";
-import PropTypes from "prop-types";
 
-
-const TaskForm = ({ newTaskTitle, setNewTaskTitle, newTaskType, setNewTaskType, handleAddTask, closeModal }) => (
-    
+const TaskForm = ({
+  newTaskTitle,
+  setNewTaskTitle,
+  newTaskType,
+  setNewTaskType,
+  handleAddTask,
+  closeModal,
+}) => (
   <form onSubmit={handleAddTask}>
     <div className="mb-4">
       <label className="block text-gray-700 mb-2">Task Title</label>
       <InputField
         type="text"
         value={newTaskTitle}
-        onChange={(e) => setNewTaskTitle(e.target.value)}
+        onChange={(e) => setNewTaskTitle(e.target.value)} // Correct usage of onChange
         className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none"
         placeholder="Enter task title"
       />
@@ -40,24 +44,20 @@ const TaskForm = ({ newTaskTitle, setNewTaskTitle, newTaskType, setNewTaskType, 
       >
         Cancel
       </Button>
-      <Button
-        type="submit"
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-      >
+      <Button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg">
         Add Task
       </Button>
     </div>
   </form>
 );
 
-// TaskForm.propTypes = {
-//     newTaskTitle : PropTypes.string,
-//     setNewTaskTitle : PropTypes.string,
-//     newTaskType : PropTypes.string,
-//     setNewTaskType : PropTypes.string,
-//     handleAddTask : PropTypes.func,
-//     closeModal : PropTypes.bool
-// }
-
+TaskForm.propTypes = {
+  newTaskTitle: PropTypes.string.isRequired,
+  setNewTaskTitle: PropTypes.func.isRequired,
+  newTaskType: PropTypes.string.isRequired,
+  setNewTaskType: PropTypes.func.isRequired,
+  handleAddTask: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
 
 export default TaskForm;
--- src/components/common/InputField.jsx
+++ src/components/common/InputField.jsx
@@ -1,22 +1,24 @@
+import React from "react";
 import PropTypes from "prop-types";
 
-const InputField = ({ value, className, placeholder, type }) => {
+const InputField = ({ value, onChange, className, placeholder, type }) => {
   return (
     <input
       type={type}
       value={value}
-      onChange={onchange}
+      onChange={onChange}
       className={className}
       placeholder={placeholder}
-    ></input>
+    />
   );
 };
 
 InputField.propTypes = {
-    value : PropTypes.string,
-    className : PropTypes.string, 
-    placeholder : PropTypes.string, 
-    type : PropTypes.string
-}
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired, // Make sure onChange is required
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+};
 
 export default InputField;
